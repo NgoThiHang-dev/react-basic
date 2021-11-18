b670b5dbebccd53e7569151f119fcad067b8d6dd
Hang Ngo Thi <1710166@dlu.edu.vn>
2021-11-18 16:44:30 +0700

    Component

---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyComponent extends React.Component{
+    render(){
+        return(
+            <div>
+                <h3>My Component (First)</h3>
+                <label>Componens chính là những viên gạch xây dựng lên bố cục của một website, cũng như là thành phần cốt lõi giúp website hoạt động.</label>
+            </div>
+        )
+    }
+}
+
+export default MyComponent;
